5a147e2d8f39943d10bd70a05977c6d8961656e2
PARAMESH8220986581 <51221478+PARAMESH8220986581@users.noreply.github.com>
2019-07-23 06:56:56 -0700

    Update romans.py

--- romans.py
+++ romans.py
@@ -1,3 +1,3 @@
-s=["I","II","III","IV",'V','VI','VII','VIII','IX','X',"XI",'XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
-a=int(input())
-print(s[a-1])
+s=["","I","II","III","IV",'V','VI','VII','VIII','IX','X',"XI",'XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+a=input()
+print(s.index(a))
