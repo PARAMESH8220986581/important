2a494faead539c9c72183a9ce6f9c5af30b137cc
PARAMESH8220986581 <51221478+PARAMESH8220986581@users.noreply.github.com>
2019-07-29 12:58:45 +0530

    Update sum in list.py

--- sum in list.py
+++ sum in list.py
@@ -1,8 +1,8 @@
 import sys, string, math
-n,k = input().split()
-n,k = int(n), int(k)
+k,n= input().split()
+k,n = int(k), int(n)
 L = [ int(x) for x in input().split()]
-for i in range(0,k) :
+for i in range(0,n) :
     a,b = input().split()
     a,b = int(a), int(b)
     print(sum(L[a-1:b]))
